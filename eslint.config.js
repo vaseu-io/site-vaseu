18a2bf46e818e0f692d062aa1f5b8e731f3ff839
vaseu-io <a.vaseu.io@gmail.com>
2026-04-24 17:18:47 -0300

    okok

--- eslint.config.js
+++ eslint.config.js
@@ -21,6 +21,13 @@ export default tseslint.config(
       ...reactHooks.configs.recommended.rules,
       "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
       "@typescript-eslint/no-unused-vars": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/ban-ts-comment": "off",
+      "@typescript-eslint/no-empty-object-type": "off",
+      "@typescript-eslint/no-unused-expressions": "off",
+      "@typescript-eslint/no-require-imports": "off",
+      "prefer-const": "off",
+      "no-empty": "off",
     },
   },
 );
